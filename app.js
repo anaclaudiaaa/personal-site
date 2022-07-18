815d6ca2174e89912d08f3c09c42c0af7650bc90
anaclaudiaaa <anacffaria@hotmail.com>
2022-07-18 22:59:37 +0100

    1st commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req,res)=>{
+    res.render("index")
+})
+
+app.get('/contacts', (req,res)=>{
+    res.render("contactus")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
